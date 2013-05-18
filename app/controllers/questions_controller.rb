1c0105df8e1392d11959a366435c00cb80d101de
Juan Figuera <juanfiguera@gmail.com>
2013-05-18 19:12:05 -0400

    add create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,14 @@ class QuestionsController < ApplicationController
   def index
   	@question = Question.new
   end
+
+  def create
+  	@question = current_user.questions.build(params[:question])
+  	if @question.save
+  		flash[:success] = "Your question has been posted!"
+  		redirect_to root_url
+  	else
+  		render 'index'
+  	end
+  end
 end
