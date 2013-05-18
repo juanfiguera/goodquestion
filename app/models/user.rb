e9c34e94b438e06f4fec4ffac73d4e5ed109d23a
Juan Figuera <juanfiguera@gmail.com>
2013-05-18 09:54:03 -0400

    edit password_digest and add validations on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  attr_accessible :password, :password_confirmation, :username
 
   has_secure_password
+
+  validates :username, presence: true, uniqueness:{ case_sensitive: false },
+  										 length: { in: 4..12 },
+  										 format: { with: /^[a-z][a-z0-9]*$/, message: 'can only contain lower-case letters and numbers'}
+  validates :password, length: { in: 4..8 }
+  validates :password_confirmation, length: { in: 4..8 }										 
+
 end
