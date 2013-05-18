72e55ff0a2cac08adcb9c83cafeb35d24b84a3c5
Juan Figuera <juanfiguera@gmail.com>
2013-05-18 08:06:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodquestion::Application.config.secret_token = '143b8e926ce0ed08e3a1e6e1f7ff04b0e43344e982a8d7bbf9643eafcc4a3ef8ec5f016ef60587e3b40eba8ba7fb3a46e6985f48cd7e5f3f5b0868048d7095ae'
