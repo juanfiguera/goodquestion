ae3487edae390302e70213fc004e5be104ad26d7
Juan Figuera <juanfiguera@gmail.com>
2013-05-18 12:34:41 -0400

    add login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  	def login(user)
+  		session[:user_id] = user.id
+  	end
+
   	def current_user
   		current_user ||= User.find(session[:user_id]) if session[:user_id]
   	end
