43adebd0853a03fac8d7c3fc983702ddefeda6de
Juan Figuera <juanfiguera@gmail.com>
2013-05-18 13:12:39 -0400

    add questions path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Goodquestion::Application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :questions, except: [:new]
+
   match '/register', to: 'users#new'
   match '/login', to: 'sessions#new'
   match '/logout', to: 'sessions#destroy', via: :delete
