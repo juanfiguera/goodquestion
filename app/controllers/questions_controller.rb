f6108fd047e9780653a081b35c1aea9e04745de8
Juan Figuera <juanfiguera@gmail.com>
2013-05-19 09:37:35 -0400

    add display_solved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,6 +37,5 @@ class QuestionsController < ApplicationController
     else
       render 'edit'
     end
-
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,8 @@ module QuestionsHelper
 	def on_your_questions_page?
 		action_name == "your_questions"
 	end
+
+	def display_solved(question)
+		(question.solved) ? ' - (Solved) ' : ''
+	end
 end
